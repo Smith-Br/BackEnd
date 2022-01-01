19472bb207608b67587ac0eecfc884058f37893f
Smith-Br <Entired.0@Gmail.com>
2022-01-01 14:38:05 +0700

    update

--- Practice Python/.py/Number/Number.py
+++ Practice Python/.py/Number/Number.py
@@ -1,1 +1,10 @@
-print(2.123); print(4 + 2); print(1 * 2.33)
+### 5.1 Integers and Floating-Point Numbers.
+## Integers
+print(type(1))
+print(type(1.0)) #! isn't integers. It's float.
+int("25") #? it'll print 25.
+# Integer literals can be written in two different ways:
+print(1000000); print(1_000_000)
+
+## Floating-Point Numbers
+print(type(1.0))
