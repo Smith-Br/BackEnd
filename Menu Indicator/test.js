319a06e760d6702a3ab58c0a9e18213ffc659178
Smith-Br <96334872+Smith-Br@users.noreply.github.com>
2022-01-06 00:17:21 +0700

    ...

--- Menu Indicator/test.js
+++ Menu Indicator/test.js
@@ -1,11 +1,24 @@
-<Picker
-    prompt={'Order status'}
-    selectedValue={status}
-    onValuehange={onUpdateStatus}>
-    {
-        STATUSES.map(status => {
-            if (status !== 'complete') return
-            return <Text>{status}</Text>
+import React from 'react'
+
+export default class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+        this.handleClick = this.handleClick.blind(this)
+    }
+    handleClick() {
+        this.setState(preState => {
+            return {
+                count: preState.count + 1
+            }
         })
     }
-</Picker>
+    render() {
+        return <div>
+            <h1>{this.state.count}</h1>
+            <button onClick={this.handleClick}>Change!</button>
+        </div>
+    }
+}
