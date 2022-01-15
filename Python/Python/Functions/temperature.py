ac670fd9b6166c030fbb7c944c914380717c3c9e
Smith-Br <Entired.0@Gmail.com>
2022-01-15 16:22:04 +0700

    Update

---
+++ Python/Python/Functions/temperature.py
@@ -0,0 +1,8 @@
+def convert_cel_to_far(C):
+    print("C --> F = " + str(C * 9/5 + 32))
+convert_cel_to_far(float(input("Your Celcius degrees: ")))
+
+
+def convert_far_to_cel(F):
+    print("D --> C = " + str((F - 32) * 5/9))
+convert_far_to_cel(float(input("Your Fahrenheit degrees: ")))
