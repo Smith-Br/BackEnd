879ae87f18d443662037046394185725699e1f57
Smith-Br <Entired.0@Gmail.com>
2022-02-01 13:15:38 +0700

    update

---
+++ Python/Python/Loops/While.py
@@ -0,0 +1,17 @@
+### --- ### --- ### --- ###
+
+#! While structure:
+
+#` 1. The while statement starts with the while keyword, followed bu a test condition, and ends with a colon (:).
+#` 2. The loop body contains the code that gets repeated at each step of the loop. Each line is indented four spaces.
+
+#$ Example:
+n = 10
+while n < 20:
+    print(n)
+    n = n + 1
+
+#. infinite loop
+while n <= 10:
+    print(n)
+
